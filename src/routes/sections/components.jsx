d7bb2c5ebdbe274768b52054957867ac281cada9
Muhammad Rafi Syahbani <mrafi.syahbani@gmail.com>
2024-10-21 12:29:08 +0700

    dist

--- src/routes/sections/components.jsx
+++ src/routes/sections/components.jsx
@@ -1,14 +1,14 @@
-// import { lazy, Suspense } from 'react';
-// import { Navigate, Outlet } from 'react-router-dom';
-// // layouts
-// import MainLayout from 'src/layouts/main';
-// // components
-// import { SplashScreen } from 'src/components/loading-screen';
+import { lazy, Suspense } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+// layouts
+import MainLayout from 'src/layouts/main';
+// components
+import { SplashScreen } from 'src/components/loading-screen';
 
-// // ----------------------------------------------------------------------
+// ----------------------------------------------------------------------
 
-// // const IndexPage = lazy(() => import('src/pages/components'));
-// // FOUNDATION
+// const IndexPage = lazy(() => import('src/pages/components'));
+// FOUNDATION
 // const ColorsPage = lazy(() => import('src/pages/components/foundation/colors'));
 // const TypographyPage = lazy(() => import('src/pages/components/foundation/typography'));
 // const ShadowsPage = lazy(() => import('src/pages/components/foundation/shadows'));
@@ -66,108 +66,108 @@
 // const MarkdownPage = lazy(() => import('src/pages/components/extra/markdown'));
 // const ScrollProgressPage = lazy(() => import('src/pages/components/extra/scroll-progress'));
 
-// // ----------------------------------------------------------------------
+// ----------------------------------------------------------------------
 
-// export const componentsRoutes = [
-//   {
-//     element: (
-//       <MainLayout>
-//         <Suspense fallback={<SplashScreen />}>
-//           <Outlet />
-//         </Suspense>
-//       </MainLayout>
-//     ),
-//     children: [
-//       {
-//         path: 'components',
-//         children: [
-//           // { element: <IndexPage />, index: true },
-//           {
-//             path: 'foundation',
-//             children: [
-//               {
-//                 element: <Navigate to="/components/foundation/colors" replace />,
-//                 index: true,
-//               },
-//               { path: 'colors', element: <ColorsPage /> },
-//               { path: 'typography', element: <TypographyPage /> },
-//               { path: 'shadows', element: <ShadowsPage /> },
-//               { path: 'grid', element: <GridPage /> },
-//               { path: 'icons', element: <IconsPage /> },
-//             ],
-//           },
-//           {
-//             path: 'mui',
-//             children: [
-//               {
-//                 element: <Navigate to="/components/mui/accordion" replace />,
-//                 index: true,
-//               },
-//               { path: 'accordion', element: <AccordionPage /> },
-//               { path: 'alert', element: <AlertPage /> },
-//               { path: 'autocomplete', element: <AutocompletePage /> },
-//               { path: 'avatar', element: <AvatarPage /> },
-//               { path: 'badge', element: <BadgePage /> },
-//               { path: 'breadcrumbs', element: <BreadcrumbsPage /> },
-//               { path: 'buttons', element: <ButtonsPage /> },
-//               { path: 'checkbox', element: <CheckboxPage /> },
-//               { path: 'chip', element: <ChipPage /> },
-//               { path: 'data-grid', element: <DataGridPage /> },
-//               { path: 'dialog', element: <DialogPage /> },
-//               { path: 'list', element: <ListPage /> },
-//               { path: 'menu', element: <MenuPage /> },
-//               { path: 'pagination', element: <PaginationPage /> },
-//               { path: 'pickers', element: <PickersPage /> },
-//               { path: 'popover', element: <PopoverPage /> },
-//               { path: 'progress', element: <ProgressPage /> },
-//               { path: 'radio-button', element: <RadioButtonsPage /> },
-//               { path: 'rating', element: <RatingPage /> },
-//               { path: 'slider', element: <SliderPage /> },
-//               { path: 'stepper', element: <StepperPage /> },
-//               { path: 'switch', element: <SwitchPage /> },
-//               { path: 'table', element: <TablePage /> },
-//               { path: 'tabs', element: <TabsPage /> },
-//               { path: 'textfield', element: <TextFieldPage /> },
-//               { path: 'timeline', element: <TimelinePage /> },
-//               { path: 'tooltip', element: <TooltipPage /> },
-//               { path: 'transfer-list', element: <TransferListPage /> },
-//               { path: 'tree-view', element: <TreesViewPage /> },
-//             ],
-//           },
-//           {
-//             path: 'extra',
-//             children: [
-//               {
-//                 element: <Navigate to="/components/extra/animate" replace />,
-//                 index: true,
-//               },
-//               { path: 'animate', element: <AnimatePage /> },
-//               { path: 'carousel', element: <CarouselsPage /> },
-//               { path: 'chart', element: <ChartPage /> },
-//               { path: 'copy-to-clipboard', element: <CopyToClipboardPage /> },
-//               { path: 'editor', element: <EditorPage /> },
-//               { path: 'form-validation', element: <FormValidationPage /> },
-//               { path: 'image', element: <ImagePage /> },
-//               { path: 'label', element: <LabelPage /> },
-//               { path: 'lightbox', element: <LightboxPage /> },
-//               { path: 'map', element: <MapPage /> },
-//               { path: 'mega-menu', element: <MegaMenuPage /> },
-//               { path: 'multi-language', element: <MultiLanguagePage /> },
-//               { path: 'navigation-bar', element: <NavigationBarPage /> },
-//               {
-//                 path: 'organization-chart',
-//                 element: <OrganizationalChartPage />,
-//               },
-//               { path: 'scroll', element: <ScrollbarPage /> },
-//               { path: 'snackbar', element: <SnackbarPage /> },
-//               { path: 'text-max-line', element: <TextMaxLinePage /> },
-//               { path: 'upload', element: <UploadPage /> },
-//               { path: 'markdown', element: <MarkdownPage /> },
-//               { path: 'scroll-progress', element: <ScrollProgressPage /> },
-//             ],
-//           },
-//         ],
-//       },
-//     ],
-//   },
-// ];
+export const componentsRoutes = [
+  {
+    element: (
+      <MainLayout>
+        <Suspense fallback={<SplashScreen />}>
+          <Outlet />
+        </Suspense>
+      </MainLayout>
+    ),
+    children: [
+      {
+        path: 'components',
+        // children: [
+        //   // { element: <IndexPage />, index: true },
+        //   {
+        //     path: 'foundation',
+        //     children: [
+        //       {
+        //         element: <Navigate to="/components/foundation/colors" replace />,
+        //         index: true,
+        //       },
+        //       { path: 'colors', element: <ColorsPage /> },
+        //       { path: 'typography', element: <TypographyPage /> },
+        //       { path: 'shadows', element: <ShadowsPage /> },
+        //       { path: 'grid', element: <GridPage /> },
+        //       { path: 'icons', element: <IconsPage /> },
+        //     ],
+        //   },
+        //   {
+        //     path: 'mui',
+        //     children: [
+        //       {
+        //         element: <Navigate to="/components/mui/accordion" replace />,
+        //         index: true,
+        //       },
+        //       { path: 'accordion', element: <AccordionPage /> },
+        //       { path: 'alert', element: <AlertPage /> },
+        //       { path: 'autocomplete', element: <AutocompletePage /> },
+        //       { path: 'avatar', element: <AvatarPage /> },
+        //       { path: 'badge', element: <BadgePage /> },
+        //       { path: 'breadcrumbs', element: <BreadcrumbsPage /> },
+        //       { path: 'buttons', element: <ButtonsPage /> },
+        //       { path: 'checkbox', element: <CheckboxPage /> },
+        //       { path: 'chip', element: <ChipPage /> },
+        //       { path: 'data-grid', element: <DataGridPage /> },
+        //       { path: 'dialog', element: <DialogPage /> },
+        //       { path: 'list', element: <ListPage /> },
+        //       { path: 'menu', element: <MenuPage /> },
+        //       { path: 'pagination', element: <PaginationPage /> },
+        //       { path: 'pickers', element: <PickersPage /> },
+        //       { path: 'popover', element: <PopoverPage /> },
+        //       { path: 'progress', element: <ProgressPage /> },
+        //       { path: 'radio-button', element: <RadioButtonsPage /> },
+        //       { path: 'rating', element: <RatingPage /> },
+        //       { path: 'slider', element: <SliderPage /> },
+        //       { path: 'stepper', element: <StepperPage /> },
+        //       { path: 'switch', element: <SwitchPage /> },
+        //       { path: 'table', element: <TablePage /> },
+        //       { path: 'tabs', element: <TabsPage /> },
+        //       { path: 'textfield', element: <TextFieldPage /> },
+        //       { path: 'timeline', element: <TimelinePage /> },
+        //       { path: 'tooltip', element: <TooltipPage /> },
+        //       { path: 'transfer-list', element: <TransferListPage /> },
+        //       { path: 'tree-view', element: <TreesViewPage /> },
+        //     ],
+        //   },
+        //   {
+        //     path: 'extra',
+        //     children: [
+        //       {
+        //         element: <Navigate to="/components/extra/animate" replace />,
+        //         index: true,
+        //       },
+        //       { path: 'animate', element: <AnimatePage /> },
+        //       { path: 'carousel', element: <CarouselsPage /> },
+        //       { path: 'chart', element: <ChartPage /> },
+        //       { path: 'copy-to-clipboard', element: <CopyToClipboardPage /> },
+        //       { path: 'editor', element: <EditorPage /> },
+        //       { path: 'form-validation', element: <FormValidationPage /> },
+        //       { path: 'image', element: <ImagePage /> },
+        //       { path: 'label', element: <LabelPage /> },
+        //       { path: 'lightbox', element: <LightboxPage /> },
+        //       { path: 'map', element: <MapPage /> },
+        //       { path: 'mega-menu', element: <MegaMenuPage /> },
+        //       { path: 'multi-language', element: <MultiLanguagePage /> },
+        //       { path: 'navigation-bar', element: <NavigationBarPage /> },
+        //       {
+        //         path: 'organization-chart',
+        //         element: <OrganizationalChartPage />,
+        //       },
+        //       { path: 'scroll', element: <ScrollbarPage /> },
+        //       { path: 'snackbar', element: <SnackbarPage /> },
+        //       { path: 'text-max-line', element: <TextMaxLinePage /> },
+        //       { path: 'upload', element: <UploadPage /> },
+        //       { path: 'markdown', element: <MarkdownPage /> },
+        //       { path: 'scroll-progress', element: <ScrollProgressPage /> },
+        //     ],
+        //   },
+        // ],
+      },
+    ],
+  },
+];
