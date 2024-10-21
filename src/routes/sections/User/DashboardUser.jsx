34919cf2049a5d4db6236f61fba5682c8b59d823
Hafidz-Second-Account <hafidzfurqonyet@gmail.com>
2024-10-21 15:43:26 +0700

    push ngadu

--- src/routes/sections/User/DashboardUser.jsx
+++ src/routes/sections/User/DashboardUser.jsx
@@ -6,11 +6,7 @@ import { LoadingScreen } from 'src/components/loading-screen';
 import UsersPage from './UsersPage';
 import DashboardUserLayout from '../dashboarduser/Layout';
 import { element } from 'prop-types';
-<<<<<<< HEAD
-import FileManagerDetailUsers from './FIleDetail/FileManagerDetailUsers';
-=======
 import { FileManagerDetailUsers } from './FileDetail/FileManagerDetailUsers';
->>>>>>> eae0407082a00742c5b25e7538271b6e0b960768
 
 export const DashboardUser = [
   {
